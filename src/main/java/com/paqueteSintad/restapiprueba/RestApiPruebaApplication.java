3f9b9fb114ccf742a7b3900819457478c9ccf5c6
ronny herrera <rdherrerah@gmail.com>
2023-03-24 14:38:25 -0500

    Proyecto base

---
+++ src/main/java/com/paqueteSintad/restapiprueba/RestApiPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.paqueteSintad.restapiprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPruebaApplication.class, args);
+	}
+
+}
